15433b11170895d7e0c240ff33cd49cf6540ea5e
vivekarya6238 <vivek.arya6238@gmail.com>
2026-05-23 09:41:51 +0530

    fix: add /api to baseURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://nearprice-backend.onrender.com",
+    baseURL: "https://nearprice-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
